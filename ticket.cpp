c13373804354db1f10473a1299b75e8884939cf8
Daniel Örhill <daniel.orhill@studerande.movant.se>
2022-11-07 10:44:51 +0100

    Added Doxygen comments

--- ticket.cpp
+++ ticket.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file ticket.cpp
+ * @author Daniel Örhill (daniel.orhill@studerande.movant.se)
+ * @brief Ticket system in C++
+ * @version 1
+ * @date 2022-11-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include <cstdlib>
 #include <list>
@@ -7,6 +17,9 @@
 #include <sstream>
 #include <unistd.h>
 
+/**
+ * @brief Flight class
+ */
 class flights
 {
     int flightN;
@@ -128,6 +141,9 @@ public:
     }
 };
 
+/**
+ * @brief Booking class
+ */
 class bookings
 {
     int bookingN;
@@ -207,6 +223,11 @@ public:
     }
 };
 
+/**
+ * @brief Function to fill flight list
+ * @param[in] file Name of flight file
+ * @param[in] List Flight list to fill
+ */
 void fillFlightList(std::string file, std::list<flights *> *List)
 {
     std::ifstream flightFile(file);
@@ -254,6 +275,11 @@ void fillFlightList(std::string file, std::list<flights *> *List)
     }
 }
 
+/**
+ * @brief Function to fill booking list
+ * @param[in] file Name of file
+ * @param[in] List Booking list to fill
+ */
 void fillBookingList(std::string file, std::list<bookings *> *List)
 {
     std::ifstream bookingFile(file);
@@ -295,6 +321,11 @@ void fillBookingList(std::string file, std::list<bookings *> *List)
     }
 }
 
+/**
+ * @brief Function to print out tickets
+ * @param[in] fList Flight list
+ * @param[in] bList Booking list
+ */
 void printTickets(std::list<flights *> fList, std::list<bookings *> bList)
 {
     std::list<bookings *>::iterator it;
@@ -348,6 +379,10 @@ void printTickets(std::list<flights *> fList, std::list<bookings *> bList)
     }
 }
 
+/**
+ * @brief Function to cancel flights with no passengers
+ * @param[in] fList Flight list
+ */
 void cancelFlights(std::list<flights *> &fList)
 {
     std::list<flights *>::iterator it;
@@ -371,6 +406,10 @@ void cancelFlights(std::list<flights *> &fList)
     cancel.close();
 }
 
+/**
+ * @brief Function to print out seating report
+ * @param[in] fList Flight list
+ */
 void seatingChart(std::list<flights *> fList)
 {
     std::list<flights *>::iterator it;
@@ -458,12 +497,26 @@ void seatingChart(std::list<flights *> fList)
     seating.close();
 }
 
+/**
+ * main entry point of the program.
+ * @param[in] argc Number of command line arguments.
+ * @param[in] argv An array of pointers to null terminated arrays of characters (command line arguments).
+ * @param[out] returns 0 on success.
+ */
 int main(int argc, char **argv)
 {
+    /**
+     * @brief Declare variables
+     */
     char *flightFile = NULL;
     char *bookingFile = NULL;
     int opt;
 
+    /**
+     * @brief Get file names with getopt function
+     * -f flight file name
+     * -b booking file name
+     */
     while ((opt = getopt(argc, argv, "f:b:?")) != -1)
     {
         switch (opt)
@@ -485,18 +538,45 @@ int main(int argc, char **argv)
         }
     }
 
+    /**
+     * @brief Declare lists
+     * flight list and booking list
+     */
     std::list<flights *> flightList;
     std::list<bookings *> bookingList;
 
     std::cout << "Checking flights...\n";
+    /**
+     * @brief Call funtion fillFlightList
+     * @param[in] flightFile Name of flight file
+     * @param[in] flightList Flight list
+     */
     fillFlightList(flightFile, &flightList);
     std::cout << "Checking bookings...\n";
+    /**
+     * @brief Call function fillBookingList
+     * @param[in] bookingFile Name of booking file
+     * @param[in] bookingList Booking list
+     */
     fillBookingList(bookingFile, &bookingList);
     std::cout << "Reserving seating...\nPrinting out flight tickets...\n";
+    /**
+     * @brief Call function printTicket
+     * @param[in] flightList Flight list
+     * @param[in] bookingList Booking list
+     */
     printTickets(flightList, bookingList);
     std::cout << "Canceling flights with no passengers...\n";
+    /**
+     * @brief Call function cancelFlights
+     * @param[in] flightList Flight list
+     */
     cancelFlights(flightList);
     std::cout << "Printing out seating report for remaining flights...\n";
+    /**
+     * @brief Call function seatingChart
+     * @param[in] flightList Flight list
+     */
     seatingChart(flightList);
 
     return 0;
